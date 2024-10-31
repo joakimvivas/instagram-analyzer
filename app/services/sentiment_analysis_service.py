2c1923c0b6749bddecf9e95f087e6ca025f9a2f4
Joakim Vivas <kim@joakimvivas.com>
2024-10-31 12:22:09 +0100

    Optimized the analyze_sentiment result

--- app/services/sentiment_analysis_service.py
+++ app/services/sentiment_analysis_service.py
@@ -5,7 +5,16 @@ sentiment_analyzer = pipeline("sentiment-analysis")
 
 def analyze_sentiment(description: str):
     if description:
-        sentiment = sentiment_analyzer(description)[0]['label']
+        result = sentiment_analyzer(description)[0]
+        label = result['label']
+        score = result['score']
+        
+        # Interpretar el resultado para agregar una categoría "NEUTRAL" cuando el score sea bajo
+        if score < 0.6:  # Umbral ajustable para definir "neutralidad"
+            sentiment = "NEUTRAL"
+        else:
+            sentiment = "POSITIVE" if label == "POSITIVE" else "NEGATIVE"
     else:
         sentiment = "NEUTRAL"
+    
     return sentiment
